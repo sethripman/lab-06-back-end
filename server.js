c89aaa91a34ef1f169b00f914471569b8ee614d9
sethripman <sethripman@gmail.com>
2019-11-05 11:02:41 -0800

    working on location

--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const geoData = require('./data/geo.json');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static('./public'));
+
+app.get('/location', (request, response) => {
+    try {
+        const location = request.query.location;
+        const result = getLatLng(location);
+        response.status(200).json(result);
+    }
+    catch (err) {
+        // TODO: make an object and send via .json...
+        response.status(500).send('Sorry something went wrong, please try again');
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+function getLatLng(location) {
+    // simulate an error if special "bad location" is provided:
+    if(location == 'bad location') {
+        throw new Error();
+    }
+
+    // ignore location for now, return hard-coded file
+    // api call will go here
+
+    // convert to desired data format:
+    return toLocation(geoData);
+}
+
+function toLocation(/*geoData*/) {
+    const firstResult = geoData.results[0];
+    const geometry = firstResult.geometry;
+    
+    return {
+        formatted_query: firstResult.formatted_address,
+        latitude: geometry.location.lat,
+        longitude: geometry.location.lng
+    };
+}
