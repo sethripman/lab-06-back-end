0e04f9cec155e6eaa2f1aa3a63208a69218d8dad
JMoneymaker <jasmin.moneymaker@gmail.com>
2019-11-05 16:31:22 -0800

    pair programming ready for api addition

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const cors = require('cors');
 const app = express();
 const geoData = require('./data/geo.json');
 const darkSky = require('./data/darksky.json');
-​
-const PORT = process.env.PORT || 3000;
-​
+
 app.use(cors());
 app.use(express.static('./public'));
-​
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
 app.get('/location', (request, response) => {
     try {
         const location = request.query.location;
@@ -21,7 +26,7 @@ app.get('/location', (request, response) => {
         response.status(500).send('Sorry something went wrong, please try again');
     }
 });
-​
+
 app.get('/weather', (request, response) => {
     try {
         const weather = request.query.weather;
@@ -33,23 +38,20 @@ app.get('/weather', (request, response) => {
         response.status(500).send('Sorry something went wrong, please try again');
     }
 });
-​
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
-​
+
+
 function getLatLng(location) {
     if (location === 'bad location') {
         throw new Error();
     }
-​
+
     // ignore location for now, return hard-coded file
     // api call will go here
-​
+
     // convert to desired data format:
     return toLocation(geoData);
 }
-​
+
 function toLocation(/*geoData*/) {
     const firstResult = geoData.results[0];
     const geometry = firstResult.geometry;
@@ -60,25 +62,25 @@ function toLocation(/*geoData*/) {
         longitude: geometry.location.lng
     };
 }
-​
+
 function getForecastTime(weather) {
     if (weather === 'bad time') {
         throw new Error();
     }
-​
+
     // ignore location for now, return hard-coded file
     // api call will go here
-​
+
     // convert to desired data format:
     return toWeather(darkSky);
 }
-​
+
 function toWeather(/*darkSky*/) {
     const days = darkSky.daily;
-​
+
     const dayArray = [];
     days.data.forEach((day) => {
-        const thisDate = new Date(day.time);
+        const thisDate = new Date(day.time * 1000);
         const timeString = thisDate.toDateString();
         const thisObject = { 
             forecast: day.summary,
@@ -88,4 +90,5 @@ function toWeather(/*darkSky*/) {
     });
     
     return dayArray;
+    
 }
